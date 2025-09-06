46191303a4dc036d816cc779a70add7ddd811870
Abdoulaye Kante <abdoulayekante@MacBook-Pro-de-Abdoulaye-2.local>
2025-09-06 16:39:29 +0200

    upd

--- js/env-config.js
+++
@@ -1,9 +0,0 @@
-// Configuration pour les API keys et autres variables sensibles
-// Ce fichier est conçu pour fonctionner avec les variables d'environnement de Netlify
-const config = {
-    // En production (sur Netlify), utilise la variable d'environnement
-    // En développement local, utilise la valeur par défaut
-    BREVO_API_KEY: typeof process !== 'undefined' && process.env.BREVO_API_KEY 
-        ? process.env.BREVO_API_KEY 
-        : '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'
-};
